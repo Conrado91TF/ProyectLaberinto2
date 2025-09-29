b5ba5bae882a3187bce90b906e688e086d0baf64
Conrado91TF <conrado_tnt@hotmail.com>
2025-09-29 20:09:24 +0100

    Parte 4 TMP, Scripts de temporizador y texto
    Creación de texto, etiquetas, scripts de cronometración

--- Assets/Scripts/DectetorMeta.cs
+++ Assets/Scripts/DectetorMeta.cs
@@ -1,9 +1,32 @@
 using UnityEngine;
+using TMPro;
 
 public class DectetorMeta : MonoBehaviour
 {
+    // Final de la meta
     [SerializeField]
     GameObject pantallaFinal;
+
+    //Etiqueta de tiempo
+
+    [SerializeField] 
+    TextMeshProUGUI textoLabeTime;
+        
+
+    // Contador de tiempo
+    float tiempoDePartida = 0.0f;
+    bool estaJugando = true;
+
+    private void Update()
+    {
+        if (estaJugando == true)
+        {
+            tiempoDePartida = tiempoDePartida + Time.deltaTime;
+
+        }
+
+    }
+    // El jugador llega a la meta
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
@@ -11,8 +34,13 @@ public class DectetorMeta : MonoBehaviour
             Debug.Log("jugador llego a la meta");
             pantallaFinal.SetActive(true);
             other.GetComponent<PlayerMovement>().enabled = false;
+            estaJugando = false;
+            textoLabeTime.text = tiempoDePartida.ToString();
 
-        
+
+
+
+
         }
 
     }
